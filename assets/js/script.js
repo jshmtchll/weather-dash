2bf36e7d016d3c66613d9777d12c1ab3801ce5c5
josh mitchell <jshmtchll@gmail.com>
2021-03-20 22:32:22 -0500

    display weather function for current day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let fiveDayForecastEl = document.getElementById("five-day-forcast");
-let currentWeaterEl = document.getElementById("current-weather");
+let currentWeatherEl = document.getElementById("current-weather");
 let currentDayTitle = document.getElementById("current-city");
 let searchHistoryEl = document.getElementById("search-history");
 let userInputEl = document.getElementById("city-input");
@@ -12,10 +12,46 @@ let searchedCities = [];
 let formSubmit = function(event) {
     event.preventDefault()
 
-    let city = userInputEl.value.trim();
-    console.log(city);
+    let city = userInputEl.value.trim().toUpperCase();
+    
+    if (city) {
+        console.log(city);
+        getLocalWeather(city);
+    }
+    else{
+        alert("Please enter a correct city");
+    }
+    
 }
 
+let getLocalWeather = function(city) {
+     
+    let api = "c9dfb03ebccab3d1072ae4c678d37794";
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + api;
+
+    fetch(url).then(function(res) {
+        res.json().then(function(data) {
+            //console.log(data);
+            displayLocalWeather(data, city) //sends data from fetch and city name to display function
+        });
+    });
+};
+
+
+let displayLocalWeather = function(data, city) {
+    currentWeatherEl.textContent = ""; //clears the current div for new content
+    console.log(data);
+    
+    
+    let currentCity = `<h2> ${city} ${moment().format("MMM D, YYYY")}<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/></h2>
+                        <div class="class-list">      
+                            <ul list-style-type = "none">
+                            <li>Temperature: ${Math.floor(data.main.temp)}</li>
+                            <ul>
+                        </div>`;
+    currentWeatherEl.innerHTML = (currentCity);
+
+}
 
 formInputEl.addEventListener("submit", formSubmit);
 
