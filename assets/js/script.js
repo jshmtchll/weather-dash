71ff0ed2bc53b4eb7848a0526298e0ce0b343b98
josh mitchell <jshmtchll@gmail.com>
2021-03-21 14:01:57 -0500

    fixed some minor css styling, added uvi and uvi color for different uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+let api = "c9dfb03ebccab3d1072ae4c678d37794";
+
 let fiveDayForecastEl = document.getElementById("five-day-forcast");
 let currentWeatherEl = document.getElementById("current-weather");
 let currentDayTitle = document.getElementById("current-city");
 let searchHistoryEl = document.getElementById("search-history");
 let userInputEl = document.getElementById("city-input");
-let formInputEl = document.getElementById("city-search")
+let formInputEl = document.getElementById("city-search");
+
 
 
 let searchedCities = [];
@@ -26,7 +29,6 @@ let formSubmit = function(event) {
 
 let getLocalWeather = function(city) {
      
-    let api = "c9dfb03ebccab3d1072ae4c678d37794";
     let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + api;
 
     fetch(url).then(function(res) {
@@ -39,7 +41,7 @@ let getLocalWeather = function(city) {
 
 
 let displayLocalWeather = function(data, city) {
-    currentWeatherEl.textContent = ""; //clears the current div for new content
+   // currentWeatherEl.textContent = ""; //clears the current div for new content
     console.log(data);
     
     
@@ -49,13 +51,55 @@ let displayLocalWeather = function(data, city) {
                             <span class="card-body text-center"><img src="./assets/images/humid.png"/> <b>Humidity:</b> ${data.main.humidity}%</span>
                             <br>
                             <span class="card-body text-center"><img src="./assets/images/wind.png"/> <b>Wind Speed:</b> ${data.wind.speed}MPH</span>
-                            <span class="card-body text-center"><img src="./assets/images/uv.png"/> <b>UV Index:</b> ${data.wind.speed}MPH</span>
+                            <span id="test" style="display:none;"><img src="./assets/images/uv.png"/> <b>UV Index:</b> <span id="uvi">  </span></span>
+                            
                         </div>`;
     currentWeatherEl.innerHTML = (currentCity);
 
+    let longitude = data.coord.lon;
+    let latitude = data.coord.lat; //get lat/lon from the single day api call and pass it to the fetch for the uvi
+    uvIndex(latitude, longitude);
+}
+
+
+let uvIndex = function(latitude, longitude) {
+    console.log(latitude);
+    console.log(longitude);
+
+    let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + api;
+
+    fetch(url).then(function(res) {
+        res.json().then(function(data) {
+            console.log(data)
+            displayUv(data) //sends to displayuv function to print on page
+        });
+    });
+}
+
+let displayUv = function(uvdata) {
+    let uvEl = document.getElementById("uvi");
+    
+    if (uvdata.current.uvi <=2) {
+        uvEl.style.backgroundColor = "#32a852"; //low uv
+    } else if (uvdata.current.uvi >2 && uvdata.current.uvi <=7) {
+        uvEl.style.backgroundColor = "#e0be14"; //high uv
+    } else if (uvdata.current.uvi >7 && uvdata.current.uvi <=10) {
+        uvEl.style.backgroundColor = "#e01414"; //very high uv
+    } else if (uvdata.current.uvi >10) {
+        uvEl.style.backgroundColor = "#a21cc7"; //severe uv
+    }
+    
+    uvEl.innerHTML = (uvdata.current.uvi);
+    document.getElementById("test").style.display = ""
+
+    
 }
 
 formInputEl.addEventListener("submit", formSubmit);
+
+
+
+
 
 
 
